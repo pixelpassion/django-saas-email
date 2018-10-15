098831b7ab7efbcfd11cc3f70c0fc3121c57446c
leotrubach <leotrubach@gmail.com>
2018-10-16 05:02:53 +0600

    Revert changes for tests.settings and urls

--- tests/urls.py
+++ tests/urls.py
@@ -2,13 +2,8 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     url(r'^', include('django_saas_email.urls', namespace='django_saas_email')),
-    url(r'^admin/', admin.site.urls)
 ]
-
-urlpatterns += staticfiles_urlpatterns()
