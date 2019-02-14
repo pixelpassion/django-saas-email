1fe71ae29269966fe05df0f1b1e07c3de91ae12e
leotrubach <leotrubach@gmail.com>
2019-02-14 06:35:24 +0600

    Add support for RQ task queue

--- django_saas_email/tasks.py
+++ django_saas_email/tasks.py
@@ -1,10 +1,19 @@
-from celery import shared_task
 from django.conf import settings
 from django.apps import apps
 from .models import Mail
 
+task_queue = getattr(settings, "DJANGO_SAAS_EMAIL_TASK_QUEUE", "celery")
+decorator_kwargs = getattr(settings, "DJANGO_SAAS_EMAIL_DECORATOR_KWARGS", {})
 
-@shared_task
+if task_queue == "celery":
+    from celery import shared_task as task_decorator
+elif task_queue == "rq":
+    from rq import job as task_decorator
+else:
+    raise RuntimeError("Only `celery` and `rq` task queues are supported.")
+
+
+@task_decorator(**decorator_kwargs)
 def send_asynchronous_mail(mail_uuid, sendgrid_api=False):
     """Send an asynchronous mail by the given ID."""
     try:
@@ -12,7 +21,7 @@ def send_asynchronous_mail(mail_uuid, sendgrid_api=False):
     except AttributeError:
         mail_model = Mail
     else:
-        app_label, model_name = app_model_name.split('.')
+        app_label, model_name = app_model_name.split(".")
         mail_model = apps.get_model(app_label, model_name)
     try:
         mail = mail_model.objects.get(id=mail_uuid)
