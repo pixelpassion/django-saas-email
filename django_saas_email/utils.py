09d7e199e848bd5dc9ae61e52e895ae8b17ad7db
Leo Trubach <leotrubach@gmail.com>
2019-04-12 01:57:57 +0600

    Fix: circular import

--- django_saas_email/utils.py
+++ django_saas_email/utils.py
@@ -1,17 +1,16 @@
-from .models import Mail
-from .tasks import send_asynchronous_mail
 from django.conf import settings
 
 
 def create_and_send_mail(**kwargs):
+    from .models import Mail
+    from .tasks import send_asynchronous_mail
+
     """Helper method to create and send a mail.
 
     create_and_send_mail(template_name="hello", context={'name': 'Jens'}, to_address="me@jensneuhaus.de")
     """
     mail = Mail.objects.create_mail(**kwargs)
-    send_asynchronous_mail.delay(
-        mail.id, sendgrid_api=getattr(settings, "SENDGRID_API_KEY", False)
-    )
+    send_asynchronous_mail.delay(mail.id, sendgrid_api=getattr(settings, "SENDGRID_API_KEY", False))
 
 
 def _noautoescape(template):
